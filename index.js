d570cfb366303db1eaa2cb262d6b74e2788d5efa
john makateto <jmakateto@gmail.com>
2023-07-16 02:57:03 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,23 +1,73 @@
-/* Your Code Here */
+// Helper Functions
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
-}
-
+function createEmployeeRecord(employeeData) {
+    return {
+      firstName: employeeData[0],
+      familyName: employeeData[1],
+      title: employeeData[2],
+      payPerHour: employeeData[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+      type: "TimeIn",
+      date: date,
+      hour: parseInt(hour, 10),
+    });
+    return this;
+  }
+  
+  function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      date: date,
+      hour: parseInt(hour, 10),
+    });
+    return this;
+  }
+  
+  function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find((event) => event.date === date);
+    let timeOut = this.timeOutEvents.find((event) => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(date) {
+    let hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+  }
+  
+  function allWagesFor() {
+    let dates = this.timeInEvents.map((event) => event.date);
+    return dates.reduce((totalWages, date) => totalWages + wagesEarnedOnDate.call(this, date), 0);
+  }
+  
+  // Main function
+  
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, employee) => totalPayroll + allWagesFor.call(employee), 0);
+  }
+  
+  // Exports
+  
+  module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    calculatePayroll,
+  };
+  
+  
--- test/helpers.js
+++ test/helpers.js
@@ -1,24 +1,23 @@
-const chai = require('chai')
-global.expect = chai.expect
-global.chai = chai
-const fs = require('file-system')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
-const spies = require('chai-spies')
+const chai = require('chai');
+global.expect = chai.expect;
+global.chai = chai;
+const fs = require('file-system');
+const jsdom = require('mocha-jsdom');
+const path = require('path');
+const babel = require('@babel/core');
+const spies = require('chai-spies');
 
-chai.use( spies );
+chai.use(spies);
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
 
-const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
-  }
-);
+const babelResult = babel.transformFileSync(path.resolve(__dirname, '..', 'index.js'), {
+  presets: ['@babel/env'],
+});
 
-const src = babelResult.code
+const src = babelResult.code;
 
 jsdom({
-  html, src
+  html,
+  src,
 });
